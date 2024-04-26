bc347364dcee138c111e9d1b1110182ff1fc72a5
Anthony Garcia <Anthony13241@gmail.com>
2024-04-26 08:20:56 +0000

    prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,62 @@
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+const sum = addTwoNumbers(2, 2);
+console.log(sum);
+
+function convertHoursToMinutes(hours) {
+  return 2 * 60;
+}
+const minutes = convertHoursToMinutes(2 * 60);
+console.log(minutes);
+
+function getGreeting(name) {
+  return getGreeting;
+}
+const greeting = getGreeting('Hello World');
+console.log('Hello' + ' ' + 'World');
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+const sumTimesFive = addAndMultiplyBy5(10, 5);
+console.log(sumTimesFive);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+const productDividedByFive = multiplyAndDivideBy5(35, 10);
+console.log(productDividedByFive);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+const difference = subtractTwoNumbers(22, 7);
+console.log(difference);
+
+function getCircleCircumference(radius) {
+  return 2 * 3.14 * 5;
+}
+const circumference = getCircleCircumference(5);
+console.log(circumference);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+const fullName = getFullName('Anthony', 'Garcia');
+console.log(fullName);
+
+function cube(number) {
+  return 5 * 5 * 5;
+}
+const cubed = cube(5);
+console.log(cubed);
